3afc03516dff4f637c3a01d54fb2afadae7976f5
Swasthik Jagadheesha <swasthik.jagadheesha@7edge.com>
2026-01-28 16:09:45 +0530

    Completed the automation for make my trip

--- ExampleAutomation/src/test/java/com/selenium/demo/base/Driver.java
+++ ExampleAutomation/src/test/java/com/selenium/demo/base/Driver.java
@@ -3,21 +3,20 @@ package com.selenium.demo.base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 public class Driver {
     protected  WebDriver driver;
 
-    @BeforeMethod
+    @BeforeClass
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.amazon.in/");
     }
 
-    @AfterMethod
+    @AfterClass
     public void tearDown(){
         if (driver != null) {
             driver.quit();
